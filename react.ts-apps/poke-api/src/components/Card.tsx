5d260fa78ab6c00ae095f58f4569a43c577a6252
gabrielNardone <gabriel.f.nardone@gmail.com>
2023-06-15 11:09:48 -0300

    some fixes

--- react.ts-apps/poke-api/src/components/Card.tsx
+++ react.ts-apps/poke-api/src/components/Card.tsx
@@ -1,17 +1,11 @@
 
-export const Card = ({ props, handleDelete }: any) => {
+export const Card = ({ props}: any) => {
     return (
         <div className="card">
             <img src={props.pic} alt={props.name} />
             <div className="card-body">
 
                 <h4><span>{props.id}</span>{props.name}</h4>
-
-                <button
-                    onClick={() => handleDelete(props.id)}
-                >
-                    Delete
-                </button>
 
             </div>
         </div>
